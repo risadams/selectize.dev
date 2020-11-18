192180f6ce27fa0891cab68286ff411f9d8c4d16
Ris Adams <chris@tuesdaybegins.com>
2020-11-18 11:02:12 -0500

    demo migrations

---
+++ js/app.js
@@ -0,0 +1,41 @@
+$(function () {
+  $("script.show").each(function () {
+    var $a, $pre;
+    var self = this;
+    var code, lines, indent, lineindent, i, n;
+
+    // re-indent code and add to <pre>
+    code = this.text;
+    if (code && code.length) {
+      lines = code.split("\n");
+      indent = null;
+
+      for (i = 0, n = lines.length; i < n; i++) {
+        if (/^[\t ]*$/.test(lines[i])) continue;
+        if (!indent) {
+          lineindent = lines[i].match(/^([\t ]+)/);
+          if (!lineindent) break;
+          indent = lineindent[1];
+        }
+        lines[i] = lines[i].replace(new RegExp("^" + indent), "");
+      }
+
+      var code = hljs.highlight("javascript", $.trim(lines.join("\n")).replace(/	/g, "    ")).value;
+      $a = $('<a href="javascript:void(0)" class="toggle-code closed">Show Code</a>');
+      $pre = $("<pre>")
+        .hide()
+        .html('<code class="javascript hljs">' + code + "</code>");
+
+      $a.on("click", function () {
+        var state = !$pre.is(":visible");
+        $pre.toggle(state);
+        $a.toggleClass("open", state);
+        $a.toggleClass("closed", !state);
+        $a.html(state ? "Hide Code" : "Show Code");
+      });
+
+      $pre.insertAfter(self);
+      $a.insertAfter(self);
+    }
+  });
+});
